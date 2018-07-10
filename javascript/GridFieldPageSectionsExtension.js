93a42698bf299ad2f589dfa92258706f6f5b93fe
Marco Crespi <mrc@flxlabs.com>
2018-07-10 12:45:16 +0200

    fix(gridfield): Fix certain drag and drop arrows not showing

--- javascript/GridFieldPageSectionsExtension.js
+++ javascript/GridFieldPageSectionsExtension.js
@@ -1,10 +1,10 @@
-(function($) {
-	$.entwine("ss", function($) {
+(function ($) {
+	$.entwine("ss", function ($) {
 		// Recursively hide a data-grid row and it's children
-		var hideRow = function($row) {
+		var hideRow = function ($row) {
 			var id = $row.data("id");
 			$("tr.ss-gridfield-item > .col-treenav[data-parent=" + id + "]").each(
-				function() {
+				function () {
 					hideRow($(this).parent());
 				}
 			);
@@ -12,7 +12,7 @@
 		};
 
 		// Hide our custom context menu when not needed
-		$(document).on("mousedown", function(event) {
+		$(document).on("mousedown", function (event) {
 			$parents = $(event.target).parents(".treenav-menu");
 			if ($parents.length == 0) {
 				$(".treenav-menu").remove();
@@ -21,13 +21,12 @@
 
 		// Show context menu
 		$(".ss-gridfield-pagesections tbody").entwine({
-			addElement: function(id, elemType) {
+			addElement: function (id, elemType) {
 				var grid = this.getGridField();
 
 				grid.reload({
 					url: grid.data("url-add"),
-					data: [
-						{
+					data: [{
 							name: "id",
 							value: id
 						},
@@ -38,12 +37,11 @@
 					]
 				});
 			},
-			removeElement: function(id, parentId, parentType) {
+			removeElement: function (id, parentId, parentType) {
 				var grid = this.getGridField();
 				grid.reload({
 					url: grid.data("url-remove"),
-					data: [
-						{
+					data: [{
 							name: "id",
 							value: id
 						},
@@ -58,28 +56,26 @@
 					]
 				});
 			},
-			deleteElement: function(id) {
+			deleteElement: function (id) {
 				var grid = this.getGridField();
 
 				grid.reload({
 					url: grid.data("url-delete"),
-					data: [
-						{
-							name: "id",
-							value: id
-						}
-					]
+					data: [{
+						name: "id",
+						value: id
+					}]
 				});
 			},
-			onadd: function() {
+			onadd: function () {
 				var grid = this.getGridField();
 				var thisGrid = this;
 
-				this.find("tr.ss-gridfield-item").each(function() {
+				this.find("tr.ss-gridfield-item").each(function () {
 					var $this = $(this);
 
 					// actions
-					$this.find(".col-actions .add-button").click(function(event) {
+					$this.find(".col-actions .add-button").click(function (event) {
 						event.preventDefault();
 						event.stopImmediatePropagation();
 						$target = $(event.target);
@@ -90,10 +86,10 @@
 
 						var $menu = $(
 							"<ul id='treenav-menu-" +
-								id +
-								"' class='treenav-menu' data-id='" +
-								id +
-								"'></ul>"
+							id +
+							"' class='treenav-menu' data-id='" +
+							id +
+							"'></ul>"
 						);
 						$menu.css({
 							top: event.pageY + "px",
@@ -103,12 +99,12 @@
 
 						$menu.append(
 							"<li class='header'>" +
-								ss.i18n._t("PageSections.GridField.AddAChild", "Add a child") +
-								"</li>"
+							ss.i18n._t("PageSections.GridField.AddAChild", "Add a child") +
+							"</li>"
 						);
-						$.each(elems, function(key, value) {
+						$.each(elems, function (key, value) {
 							var $li = $("<li data-type='" + key + "'>" + value + "</li>");
-							$li.click(function() {
+							$li.click(function () {
 								thisGrid.addElement(rowId, key);
 								$menu.remove();
 							});
@@ -117,7 +113,7 @@
 						$menu.show();
 					});
 
-					$this.find(".col-actions .delete-button").click(function(event) {
+					$this.find(".col-actions .delete-button").click(function (event) {
 						event.preventDefault();
 						event.stopImmediatePropagation();
 
@@ -130,10 +126,10 @@
 
 						var $menu = $(
 							"<ul id='treenav-menu-" +
-								id +
-								"' class='treenav-menu' data-id='" +
-								id +
-								"'></ul>"
+							id +
+							"' class='treenav-menu' data-id='" +
+							id +
+							"'></ul>"
 						);
 						$menu.css({
 							top: event.pageY + "px",
@@ -143,16 +139,16 @@
 
 						$menu.append(
 							"<li class='header'>" +
-								ss.i18n._t("PageSections.GridField.Delete", "Delete") +
-								"</li>"
+							ss.i18n._t("PageSections.GridField.Delete", "Delete") +
+							"</li>"
 						);
 
 						var $li = $(
 							"<li data-type='__REMOVE__'>" +
-								ss.i18n._t("PageSections.GridField.RemoveAChild", "Remove") +
-								"</li>"
+							ss.i18n._t("PageSections.GridField.RemoveAChild", "Remove") +
+							"</li>"
 						);
-						$li.click(function() {
+						$li.click(function () {
 							thisGrid.removeElement(rowId, parentId, parentType);
 							$menu.remove();
 						});
@@ -160,13 +156,13 @@
 						if ($target.data("used-count") < 2) {
 							var $li = $(
 								"<li>" +
-									ss.i18n._t(
-										"PageSections.GridField.DeleteAChild",
-										"Finally delete"
-									) +
-									"</li>"
+								ss.i18n._t(
+									"PageSections.GridField.DeleteAChild",
+									"Finally delete"
+								) +
+								"</li>"
 							);
-							$li.click(function() {
+							$li.click(function () {
 								thisGrid.deleteElement(rowId, $menu.data("parent-id"));
 								$menu.remove();
 							});
@@ -176,6 +172,9 @@
 						$menu.show();
 					});
 
+					/*$this.after("<tr class='pagesections-droprow'>" +
+						"<td class='col-treenav' colspan='999'></td></tr>");*/
+
 					// reorder
 					var arrowIcon =
 						"<svg width='20' height='15' xmlns='http://www.w3.org/2000/svg'><path d='M10.957 10.882v2.367a1.21 1.21 0 0 1-1.905.988l-8.54-6.02a1.21 1.21 0 0 1 0-1.976l8.54-6.02a1.21 1.21 0 0 1 1.906.988v2.418h7.254c.668 0 1.21.542 1.21 1.21v4.836a1.21 1.21 0 0 1-1.21 1.209h-7.255z' fill='#4A4A4A' fill-rule='evenodd'/></svg>";
@@ -183,18 +182,18 @@
 					$col = $this.find(".col-reorder");
 					$col.append(
 						"<div class='before'>" +
-							arrowIcon +
-							"</div><div class='middle'>" +
-							arrowIcon +
-							"</div><div class='after'>" +
-							arrowIcon +
-							"</div>"
+						arrowIcon +
+						"</div><div class='middle'>" +
+						arrowIcon +
+						"</div><div class='after'>" +
+						arrowIcon +
+						"</div>"
 					);
-					$col.find("div").each(function() {
+					$col.find("div").each(function () {
 						$(this).droppable({
 							hoverClass: "state-active",
 							tolerance: "pointer",
-							drop: function(event, ui) {
+							drop: function (event, ui) {
 								$drop = $(this);
 
 								var type = "before";
@@ -227,8 +226,7 @@
 
 								grid.reload({
 									url: grid.data("url-reorder"),
-									data: [
-										{
+									data: [{
 											name: "type",
 											value: type
 										},
@@ -274,27 +272,32 @@
 						hoverClass: "state-active",
 						tolerance: "pointer",
 						greedy: true,
-						helper: function() {
+						helper: function () {
 							var $helper = $(
 								"<div class='col-treenav__draggable'>" +
-									$this.find(".col-treenav__title").text() +
-									"</div>"
+								$this.find(".col-treenav__title").text() +
+								"</div>"
 							);
 							$this.css("opacity", 0.6);
 
 							return $helper;
 						},
-						start: function() {
+						start: function () {
 							var element = $this.data("class");
-							$(".ui-droppable").each(function() {
+							$(".ui-droppable").each(function () {
 								var $drop = $(this);
 								var $treenav = $drop.parent().siblings(".col-treenav");
 								var isOpen = $treenav.find("button").hasClass("is-open");
 								var $tr = $drop.parents("tr.ss-gridfield-item");
 
 								// Check if we're allowed to drop the element on the specified drop point.
+
 								// dont enable dropping on itself
-								if ($tr.data("id") == $this.data("id")) return;
+								if ($tr.data("id") == $this.data("id") &&
+									$treenav.siblings(".col-reorder").data("sort") == $this.find(".col-reorder").data("sort") &&
+									$treenav.data("parent") == $this.find(".col-treenav").data("parent")) {
+									return;
+								}
 
 								// dont enable dropping on .before of itself
 								if (
@@ -303,29 +306,42 @@
 								) {
 									return;
 								}
-								// dont enable dropping on .after if we are open and the next element is us
-								else if (
+
+								// dont enable dropping on .after of itself
+								if (
 									$drop.hasClass("after") &&
-									isOpen &&
 									$tr.next().data("id") == $this.data("id")
 								) {
 									return;
 								}
-								// Depending on where we drop it (before, middle or after) we have to either
-								// don't show middle if open
-								else if ($drop.hasClass("middle") && isOpen) {
+
+								// dont enable dropping on .middle of other same id elements
+								if (
+									$drop.hasClass("middle") &&
+									$tr.data("id") == $this.data("id")
+								) {
 									return;
 								}
+
+								// Depending on where we drop it (before, middle or after) we have to either
+								// don't show middle if open
+								if ($drop.hasClass("middle") && isOpen) {
+									return;
+								}
+
 								// let's handle level 0
-								else if (
+								if (
 									$treenav.data("level") == 0 &&
 									($drop.hasClass("before") ||
 										($drop.hasClass("after") && !isOpen))
 								) {
-									if (!$this.data("allowed-root")) return;
+									if (!$this.find(".col-treenav").data("allowed-root")) {
+										return;
+									}
 								}
+
 								// check our allowed children, or the allowed children of our parent row.
-								else if (
+								if (
 									$drop.hasClass("before") ||
 									($drop.hasClass("after") && !isOpen)
 								) {
@@ -346,7 +362,7 @@
 								$(this).show();
 							});
 						},
-						stop: function(event, ui) {
+						stop: function (event, ui) {
 							$(".ui-droppable").hide();
 							// Show the previous elements. If the user made an invalid movement then
 							// we want this to show anyways. If he did something valid the grid will
@@ -356,7 +372,7 @@
 					});
 				});
 			},
-			onremove: function() {
+			onremove: function () {
 				if (this.data("sortable")) {
 					this.sortable("destroy");
 				}
