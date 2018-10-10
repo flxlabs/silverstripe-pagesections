3c51ffe7853abbbc6a53523c584754246f15d5b3
Marco (Valandur) <inithilian@gmail.com>
2018-10-10 14:37:58 +0200

    fix(publish): Fix changing sub elements not marking the section changed

--- src/PageElement.php
+++ src/PageElement.php
@@ -131,6 +131,12 @@ class PageElement extends DataObject
 	{
 		parent::onBeforeWrite();
 
+		// If a  field changed then update the counter, unless it's the counter that changed
+		$changed = $this->getChangedFields(true, DataObject::CHANGE_VALUE);
+		if (count($changed) > 0 && (!isset($changed["__Counter"]) || $changed["__Counter"]["level"] <= 1)) {
+			$this->__Counter++;
+		}
+
 		$elems = $this->Children()->Sort("SortOrder")->Column("ID");
 		$count = count($elems);
 		for ($i = 0; $i < $count; $i++) {
